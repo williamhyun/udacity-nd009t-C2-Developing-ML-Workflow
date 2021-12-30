5d40cf85cf10b063ab4b0ca6f02e8952ac4df418
williamhyun <william@apache.org>
2021-12-30 08:39:42 +0000

    Working step function

---
+++ project/Lambda Functions/Image Classifier/lambda_function.py
@@ -0,0 +1,33 @@
+# Image Classifier
+
+import json
+import sagemaker
+import base64
+from sagemaker.serializers import IdentitySerializer
+
+# Fill this in with the name of your deployed model
+ENDPOINT = 'image-classification-2021-12-30-04-39-21-342'
+
+def lambda_handler(event, context):
+
+    # Decode the image data
+    image = base64.b64decode(event["body"]["image_data"])
+
+    # Instantiate a Predictor
+    predictor = sagemaker.predictor.Predictor(ENDPOINT)
+
+    # For this model the IdentitySerializer needs to be "image/png"
+    predictor.serializer = IdentitySerializer("image/png")
+    
+    # Make a prediction:
+    inferences = predictor.predict(image)
+    
+    # We return the data back to the Step Function    
+    event["inferences"] = inferences.decode('utf-8')
+    return {
+        'statusCode': 200,
+        'body': json.dumps(event)
+    }
+
+
+
